673913aea1a9fef224ee5597b8736d31bf5ca2a7
Joong-Won Seo <joong-won.seo@tum.de>
2020-10-29 13:47:07 +0100

    Exercise-Template pushed by Artemis

---
+++ src/pgdp/pinguracing/PinguRacing.java
@@ -0,0 +1,9 @@
+package pgdp.pinguracing;
+
+import static pgdp.MiniJava.*; //Diesen Import nicht löschen! Do not delete this import!
+
+public class PinguRacing {
+    public static void main(String[] args) {
+
+    }
+}
